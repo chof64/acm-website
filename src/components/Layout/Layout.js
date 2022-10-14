0a58f31b14b249d691c8c1411897585354a8b6d6
Chad Fernandez <chadfernandez@duck.com>
2022-10-14 16:24:55 +0800

    added navigation bar on pages

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,6 +1,11 @@
+import Navigation from "./Navigation";
+
 export default function Layout({ children }) {
   return (
     <main className="subpixel-antialiased">
+      <div className="sticky top-0 z-[100]">
+        <Navigation />
+      </div>
       <div>{children}</div>
     </main>
   );
