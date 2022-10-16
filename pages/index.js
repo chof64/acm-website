027fb6e304139f56956e96cb87c68600edc5d811
Chad Fernandez <chadfernandez@duck.com>
2022-10-16 19:18:58 +0800

    added notion integration `latest_information`

--- pages/index.js
+++ pages/index.js
@@ -7,9 +7,20 @@ import LayoutGlobal from "/src/components/Layout/LayoutGlobal";
 import Layout from "/src/components/Layout/Layout";
 import Platform from "/src/components/Layout/Platform";
 
+import { latestInformation } from "/src/packages/notion-api/LatestInformation";
+
 import { classMerge } from "/src/utils/TailwindUtilities";
 
-export default function Index() {
+export const getStaticProps = async () => {
+  return {
+    props: {
+      latest_information: await latestInformation(),
+    },
+    revalidate: 10,
+  };
+};
+
+export default function Index({ latest_information }) {
   return (
     <>
       <Platform>
@@ -54,60 +65,41 @@ export default function Index() {
               Latest Information
             </p>
             <Tab.List className="mt-2 grid grid-rows-3 gap-1 pb-1 font-semibold md:w-[30%]">
-              <Tab as={Fragment}>
-                {({ selected }) => (
-                  <button
-                    className={classMerge(
-                      "rounded-lg p-2 text-left",
-                      selected ? "bg-white text-teal-600" : "text-gray-400"
-                    )}
-                  >
-                    Announcements
-                  </button>
-                )}
-              </Tab>
-              <Tab as={Fragment}>
-                {({ selected }) => (
-                  <button
-                    className={classMerge(
-                      "rounded-lg p-2 text-left",
-                      selected ? "bg-white text-teal-600" : "text-gray-400"
-                    )}
-                  >
-                    Blog
-                  </button>
-                )}
-              </Tab>
-              <Tab as={Fragment}>
-                {({ selected }) => (
-                  <button
-                    className={classMerge(
-                      "rounded-lg p-2 text-left",
-                      selected ? "bg-white text-teal-600" : "text-gray-400"
-                    )}
-                  >
-                    Events
-                  </button>
-                )}
-              </Tab>
+              {latest_information.map((item, index) => (
+                <Tab key={index} as={Fragment}>
+                  {({ selected }) => (
+                    <button
+                      className={classMerge(
+                        "rounded-lg p-2 text-left",
+                        selected ? "bg-white text-teal-600" : "text-gray-400"
+                      )}
+                    >
+                      {item.name}
+                    </button>
+                  )}
+                </Tab>
+              ))}
             </Tab.List>
             <Tab.Panels className="rounded-lg bg-white p-2 pt-1 shadow-inner md:w-[70%]">
-              <Tab.Panel className="">
-                <h2 className="text-xs font-bold text-gray-400 uppercase">
-                  Latest Announcements
-                </h2>
-                {/* Add list of announcements from CMS here. */}
-              </Tab.Panel>
-              <Tab.Panel>
-                <h2 className="text-xs font-bold text-gray-400 uppercase">
-                  Blog Posts
-                </h2>
-              </Tab.Panel>
-              <Tab.Panel>
-                <h2 className="text-xs font-bold text-gray-400 uppercase">
-                  Scheduled Events
-                </h2>
-              </Tab.Panel>
+              {latest_information.map((item, index) => (
+                <Tab.Panel key={index} className="">
+                  <h2 className="text-xs font-bold text-gray-400 uppercase">
+                    {item.header}
+                  </h2>
+                  {/* Add list of announcements from CMS here. */}
+                  <div>
+                    {item.data.map((item) => (
+                      <div key={item.id} className="mt-2">
+                        <Link href={`/latest-information/${item.slug}`}>
+                          <a className="text-sm font-semibold text-gray-700">
+                            {item.properties.Name.title[0].plain_text}
+                          </a>
+                        </Link>
+                      </div>
+                    ))}
+                  </div>
+                </Tab.Panel>
+              ))}
             </Tab.Panels>
           </Tab.Group>
         </div>
