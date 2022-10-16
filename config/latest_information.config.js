cdb2facebdf6c9bf48fc864d0c7911e7dc849f1c
Chad Fernandez <chadfernandez@duck.com>
2022-10-16 19:18:17 +0800

    add `latest_information` config file

---
+++ config/latest_information.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  tabs: [
+    {
+      name: "Announcements",
+      header: "Latest Announcements",
+      notion_category: "Announcements",
+      data_key: "announcements",
+    },
+    {
+      name: "Blog",
+      header: "Latest Blog Posts",
+      notion_category: "Blog",
+      data_key: "blog",
+    },
+    {
+      name: "Events",
+      header: "Scheduled Events",
+      notion_category: "Events",
+      data_key: "events",
+    },
+  ],
+};
