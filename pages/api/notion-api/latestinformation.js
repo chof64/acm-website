02d133fed24537dcc51dff7938929b1918c82a77
Chad Fernandez <chadfernandez@duck.com>
2022-10-19 10:57:30 +0800

    refactor conflicting names

--- pages/api/notion-api/latestinformation.js
+++ pages/api/notion-api/latestinformation.js
@@ -1,7 +1,7 @@
-import { structuredLatestInformation } from "/src/packages/notion-api/LatestInformation";
+import { latestInformation } from "/src/packages/notion-api/LatestInformation";
 
-export default async function latestInformation(req, res) {
+export default async function LatestInformation(req, res) {
   res.status(200).json({
-    latestInformation: await structuredLatestInformation(),
+    latestInformation: await latestInformation(),
   });
 }
