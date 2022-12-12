521009ade48746fc65d68abac6e8fd70b5957f8b
Chad Fernandez <contact@chadfernandez.me>
2022-12-12 13:34:15 +0000

    updated Platform to take into consideration height values

--- src/components/Platform.js
+++ src/components/Platform.js
@@ -4,8 +4,8 @@ import { classMerge } from "/src/utils/TailwindUtilities";
 
 export default function Platform({ children, className }) {
   return (
-    <div className={classMerge("flex flex-col items-center", className)}>
-      <div className="w-[95vw] md:w-[90vw] lg:w-[90vw]">{children}</div>
+    <div className={classMerge("flex h-full flex-col items-center", className)}>
+      <div className="h-full w-[95vw] md:w-[90vw] lg:w-[90vw]">{children}</div>
     </div>
   );
 }
